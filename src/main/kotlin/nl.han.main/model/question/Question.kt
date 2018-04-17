a586e9219d903c7327fb78a26bbba294d8adcbb4
yarince <yarince.martis@gmail.com>
2018-04-17 10:39:32 +0200

    Changed by review comments

--- src/main/kotlin/nl.han.main/model/question/Question.kt
+++ src/main/kotlin/nl.han.main/model/question/Question.kt
@@ -3,7 +3,6 @@ package nl.han.main.model.question
 abstract class Question (
         val questionId: Int,
         val questionText: String? = null,
+        val questionType: QuestionType? = null,
         val subQuestions: Array<Question>? = null
-        ) {
-val questionType = QuestionType.OPEN_QUESTION
-}
+        )
