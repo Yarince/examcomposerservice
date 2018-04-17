68fc204c6ae05ff3645339d9c09966648e4765ec
Milo Tolboom <milotolboom@gmail.com>
2018-04-17 15:36:11 +0200

    fixed openQuestion that didnt implement the new question object fully

--- src/main/kotlin/nl.han.main/model/question/OpenQuestion.kt
+++ src/main/kotlin/nl.han.main/model/question/OpenQuestion.kt
@@ -3,8 +3,12 @@ package nl.han.main.model.question
 class OpenQuestion(
         questionId: Int,
         questionText: String?,
+        course: String,
+        examType: ExamType,
         subQuestions: Array<Question>?) :
         Question(questionId,
                 questionText,
-                QuestionType.OPEN_QUESTION, // Hard coded a open question is always an OPEN_QUESTION
+                QuestionType.OPEN_QUESTION, // Hard coded a open question is always an OPEN_QUESTION,
+                course,
+                examType,
                 subQuestions)
--- src/main/kotlin/nl.han.main/model/question/Question.kt
+++ src/main/kotlin/nl.han.main/model/question/Question.kt
@@ -12,7 +12,7 @@ open class Question(
         @ApiModelProperty(notes = "The course that the question is for. For example: APP or SWA")
         val course: String,
         @ApiModelProperty(notes = "The type of the exam. This could be an EXAM, or a PRACTICE_EXAM")
-        val examType: ExamType? = ExamType.EXAM,
+        val examType: ExamType,
         @ApiModelProperty(notes = "A question can contain subquestions when the type is noQuestion")
         val subQuestions: Array<Question>? = null
 )
