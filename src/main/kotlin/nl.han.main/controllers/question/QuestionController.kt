0e9f2ca6e90dcdccc90ed31e6c95e303d381fb81
Milo Tolboom <milotolboom@gmail.com>
2018-04-18 09:22:42 +0200

    added question service and a responseEntity. Also made a setup for questionDAO

--- src/main/kotlin/nl.han.main/controllers/question/QuestionController.kt
+++ src/main/kotlin/nl.han.main/controllers/question/QuestionController.kt
@@ -1,7 +1,9 @@
-package nl.han.main.service.question
+package nl.han.main.controllers.question
 
 import io.swagger.annotations.*
 import nl.han.main.model.question.Question
+import nl.han.main.service.question.QuestionService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -11,10 +13,14 @@ import org.springframework.web.bind.annotation.RestController
 @Api("question", description = "Creating, updating and deleting questions")
 class QuestionController {
 
+    @Autowired
+    lateinit var questionService: QuestionService
+
     @RequestMapping("/question", method = [RequestMethod.POST])
     @ApiOperation(value = "Create a question", notes = "Create a question")
     @ApiResponses(
-            ApiResponse(code = 200, message = "Success")
+            ApiResponse(code = 201, message = "Created"),
+            ApiResponse(code = 500, message = "Internal server error")
     )
-    fun createQuestion(@ApiParam(value = "Question object", required = true) @RequestBody requestBodyJson: Question) = requestBodyJson
+    fun createQuestion(@ApiParam(value = "Question object", required = true) @RequestBody question: Question) = questionService.addQuestion(question)
 }
---
+++ src/main/kotlin/nl.han.main/service/question/QuestionService.kt
@@ -0,0 +1,19 @@
+package nl.han.main.service.question
+
+import nl.han.main.model.question.Question
+import nl.han.main.persistence.question.QuestionDAO
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+
+@Service
+class QuestionService {
+    fun addQuestion(question: Question): ResponseEntity<Question> {
+        return try {
+            QuestionDAO.insertQuestion(question)
+            ResponseEntity(question, HttpStatus.CREATED)
+        } catch (exception: Exception) {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+}
