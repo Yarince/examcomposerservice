347c70bd13770eea8a0b3b6b2dc3c82fa21e94be
Milo Tolboom <milotolboom@gmail.com>
2018-04-18 09:50:49 +0200

    added a test for questioncontroller

--- src/main/kotlin/nl.han.main/controllers/question/QuestionController.kt
+++ src/main/kotlin/nl.han.main/controllers/question/QuestionController.kt
@@ -4,10 +4,7 @@ import io.swagger.annotations.*
 import nl.han.main.model.question.Question
 import nl.han.main.service.question.QuestionService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @Api("question", description = "Creating, updating and deleting questions")
@@ -16,7 +13,7 @@ class QuestionController {
     @Autowired
     lateinit var questionService: QuestionService
 
-    @RequestMapping("/question", method = [RequestMethod.POST])
+    @PostMapping("/question")
     @ApiOperation(value = "Create a question", notes = "Create a question")
     @ApiResponses(
             ApiResponse(code = 201, message = "Created"),
---
+++ test/nl/han/main/controllers/question/QuestionControllerTes.kt
@@ -0,0 +1,42 @@
+package nl.han.main.controllers.question
+
+import nl.han.main.model.question.ExamType
+import nl.han.main.model.question.Question
+import nl.han.main.model.question.QuestionType
+import nl.han.main.service.question.QuestionService
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.doReturn
+import org.mockito.junit.MockitoJUnitRunner
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import kotlin.test.assertEquals
+
+@RunWith(MockitoJUnitRunner::class)
+class ExamControllerTest {
+
+    @InjectMocks
+    private
+    lateinit var questionController: QuestionController
+
+    @Mock
+    private
+    lateinit var questionService: QuestionService
+
+    @Test
+    fun testGetExams() {
+        val expectedResultBody = Question(1, "", QuestionType.OPEN_QUESTION, "", null, ExamType.PRACTICE_EXAM, null)
+        val expectedStatusCode = HttpStatus.CREATED
+
+        doReturn(ResponseEntity(expectedResultBody, expectedStatusCode)).`when`(questionService).addQuestion(expectedResultBody)
+
+        val result = questionController.createQuestion(expectedResultBody)
+
+        assertNotNull(result)
+        assertEquals(expectedResultBody, result.body)
+        assertEquals(expectedStatusCode, result.statusCode)
+    }
+}
