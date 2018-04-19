a83daa13a51e93381eac594f4fb27afe9be2e51f
Milo Tolboom <milotolboom@gmail.com>
2018-04-19 14:06:30 +0200

    improved get question with a 404 exception

--- src/main/kotlin/nl.han.ica/examplatform/business/exam/ExamService.kt
+++ src/main/kotlin/nl.han.ica/examplatform/business/exam/ExamService.kt
@@ -1,5 +1,6 @@
 package nl.han.ica.examplatform.business.exam
 
+import nl.han.ica.examplatform.controllers.responseExceptions.InvalidExamException
 import nl.han.ica.examplatform.models.exam.Exam
 import nl.han.ica.examplatform.models.exam.ExamType
 import nl.han.ica.examplatform.persistence.exam.ExamDAOStub
--- src/main/kotlin/nl.han.ica/examplatform/persistence/exam/ExamDAOStub.kt
+++ src/main/kotlin/nl.han.ica/examplatform/persistence/exam/ExamDAOStub.kt
@@ -1,5 +1,6 @@
 package nl.han.ica.examplatform.persistence.exam
 
+import nl.han.ica.examplatform.controllers.responseExceptions.ExamNotFoundException
 import nl.han.ica.examplatform.models.exam.Exam
 import nl.han.ica.examplatform.models.exam.ExamType
 import org.springframework.stereotype.Repository
@@ -10,8 +11,15 @@ class ExamDAOStub {
     fun insertExam(exam: Exam): Exam {
         return exam
     }
-    fun getExam(id: Int): Exam {
-        return Exam(examId = id, durationInMinutes = 10, startTime = Date(6000), course = "APP", examType = ExamType.EXAM, name = "Exam 1")
-    }
 
+    fun getExam(id: Int): Exam {
+        // Database query on exam with this ID.
+        // If it does not exist, throw: ExamNotFoundException
+        // For testing purposes, ID -9999 throws a not found.
+        // This has to be changed later
+        if (id == -9999) {
+            throw ExamNotFoundException("Exam with id $id was not found.")
+        }
+        return Exam(examId = id, durationInMinutes = 10, startTime = Date(6000), course = "APP", examType = ExamType.EXAM, name = "Exam $id")
+    }
 }
