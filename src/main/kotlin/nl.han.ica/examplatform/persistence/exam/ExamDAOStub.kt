9f94348a529cb092d9a1ee826b57c9071d5b42c7
Milo Tolboom <milotolboom@gmail.com>
2018-04-19 15:58:00 +0200

    added extra fields in exam to be complete when get is invoked

--- src/main/kotlin/nl.han.ica/examplatform/persistence/exam/ExamDAOStub.kt
+++ src/main/kotlin/nl.han.ica/examplatform/persistence/exam/ExamDAOStub.kt
@@ -3,6 +3,8 @@ package nl.han.ica.examplatform.persistence.exam
 import nl.han.ica.examplatform.controllers.responseExceptions.ExamNotFoundException
 import nl.han.ica.examplatform.models.exam.Exam
 import nl.han.ica.examplatform.models.exam.ExamType
+import nl.han.ica.examplatform.models.question.Question
+import nl.han.ica.examplatform.models.question.QuestionType
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -20,6 +22,25 @@ class ExamDAOStub {
         if (id == -9999) {
             throw ExamNotFoundException("Exam with id $id was not found.")
         }
-        return Exam(examId = id, durationInMinutes = 10, startTime = Date(6000), course = "APP", examType = ExamType.EXAM, name = "Exam $id")
+
+        // Here the DAO should query an exam, containing exam meta data, questions and answers
+        // These values should then be put into an Exam object
+        return Exam(examId = id,
+                durationInMinutes = 10,
+                startTime = Date(6000),
+                course = "APP",
+                examType = ExamType.EXAM,
+                name = "Exam $id",
+                location = "Nijmegen",
+                instructions = "Use of a calculator is prohibited",
+                questions = arrayOf(Question(questionId = 1,
+                        questionType = QuestionType.OPEN_QUESTION,
+                        questionText = "Explain why DCAR is better",
+                        examType = ExamType.EXAM), Question(questionId = 2,
+                        questionType = QuestionType.MULTIPLE_CHOICE_QUESTION,
+                        questionText = "Choose between A, B and C",
+                        examType = ExamType.EXAM)
+                )
+        )
     }
 }
