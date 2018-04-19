f2df1af985d4a4e197978496372f1d2cfd7ce2ad
Arthur Baars <A.Baars@student.han.nl>
2018-04-19 13:17:14 +0200

    Deleted old .properties and entered placeholders.

--- src/main/kotlin/nl.han.ica/examplatform/Application.kt
+++ src/main/kotlin/nl.han.ica/examplatform/Application.kt
@@ -1,11 +1,8 @@
 package nl.han.ica.examplatform
 
-import nl.han.ica.examplatform.persistence.databaseconnection.MySQLConnection
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import springfox.documentation.swagger2.annotations.EnableSwagger2
-
-
 
 @SpringBootApplication
 @EnableSwagger2
