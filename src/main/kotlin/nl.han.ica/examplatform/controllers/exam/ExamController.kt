c075b622b8cb4a162d90b56476f032cb31a2ccf7
Milo Tolboom <milotolboom@gmail.com>
2018-04-23 12:47:46 +0200

    changed mappings after review of robin

--- src/main/kotlin/nl.han.ica/examplatform/controllers/exam/ExamController.kt
+++ src/main/kotlin/nl.han.ica/examplatform/controllers/exam/ExamController.kt
@@ -13,18 +13,18 @@ import org.springframework.web.bind.annotation.*
 
 
 @RestController
-@RequestMapping("exam")
+@RequestMapping("exams")
 class ExamController {
 
     //Load examService as Spring Bean
     @Autowired
     lateinit var examService: ExamService
 
-    @GetMapping("/all")
+    @GetMapping
     @ApiOperation(value = "Get a list of minified exams", notes = "This returns a list of exams containing ID, name, ", response = Array<SimpleExam>::class)
     fun getExams() = examService.getExams()
 
-    @PostMapping()
+    @PostMapping
     @ApiOperation(value = "Add an empty exam", notes = "Cannot contain questions or an examId", response = ResponseEntity::class)
     @ApiResponses(
             ApiResponse(code = 201, message = "Create"),
@@ -33,13 +33,13 @@ class ExamController {
     fun addExam(@RequestBody exam: Exam): ResponseEntity<Exam> =
             examService.addExam(exam)
 
-    @GetMapping()
+    @GetMapping("/{id}")
     @ApiOperation(value = "Retrieve a specific exam", notes = "Retrieve a specific exam, containing all information, questions and answers", response = Exam::class)
     @ApiResponses(
             ApiResponse(code = 201, message = "Create"),
             ApiResponse(code = 403, message = "Bad request"),
             ApiResponse(code = 404, message = "Not found")
     )
-    fun getExam(@ApiParam(value = "The ID of the exam you want to retrieve", required = true) @RequestParam id: Int): ResponseEntity<Exam> =
+    fun getExam(@ApiParam(value = "The ID of the exam you want to retrieve", required = true) @PathVariable("id") id: Int): ResponseEntity<Exam> =
             examService.getExam(id)
 }
