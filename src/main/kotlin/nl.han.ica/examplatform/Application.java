5f10c14c01c7c6818c7b9287362679b7cf442c5c
Milo Tolboom <milotolboom@gmail.com>
2018-04-24 11:14:41 +0200

    made it so the kotlin spring application runs with docker

--- src/main/kotlin/nl.han.ica/examplatform/Application.java
+++ src/main/kotlin/nl.han.ica/examplatform/Application.java
@@ -2,15 +2,17 @@ package nl.han.ica.examplatform;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableSwagger2
-public class App {
+public class Application {
 
     public static void main(String[] args) {
-//        SpringApplication.run(App.class, args);
-        System.out.println("Hello World!");
+        System.out.println("Hello world2");
+        SpringApplication.run(Application.class, args);
     }
 }
--- src/main/kotlin/nl.han.ica/examplatform/Application.kt
+++ src/main/kotlin/nl.han.ica/examplatform/Application.kt
@@ -1,16 +1,19 @@
-package nl.han.ica.examplatform
-
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import springfox.documentation.swagger2.annotations.EnableSwagger2
-
-@SpringBootApplication
-@EnableSwagger2
-class Application {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
-        }
-    }
-}
+//@file:JvmName("Launcher")
+//package nl.han.ica.examplatform
+//
+//import org.springframework.boot.SpringApplication
+//import org.springframework.boot.autoconfigure.SpringBootApplication
+//import springfox.documentation.swagger2.annotations.EnableSwagger2
+//
+//
+//@SpringBootApplication
+//@EnableSwagger2
+//class Application {
+//    companion object {
+//        @JvmStatic
+//        fun main(args: Array<String>) {
+//            //SpringApplication.run(Application::class.java, *args)
+//            print("Hello world")
+//        }
+//    }
+//}
