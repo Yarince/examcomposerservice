f408e30b07632abcfd24ac6f2a386834fa5b1bbb
YN.Martis@student.han.nl <YN.Martis@student.han.nl>
2018-04-24 15:08:05 +0200

    Added integration test for AddQuestionToExam (Had to overwrite question array in Exam.kt & Question.kt

--- integrationtest/nl/han/ica/examplatform/controllers/ExamControllerIntegrationTest.kt
+++ integrationtest/nl/han/ica/examplatform/controllers/ExamControllerIntegrationTest.kt
@@ -3,18 +3,18 @@ package nl.han.ica.examplatform.controllers
 import junit.framework.TestCase.*
 import nl.han.ica.examplatform.models.exam.Exam
 import nl.han.ica.examplatform.models.exam.ExamType
+import nl.han.ica.examplatform.models.question.Question
+import nl.han.ica.examplatform.models.question.QuestionType
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.PropertySource
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
+import org.springframework.http.*
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.exchange
 import org.springframework.web.client.getForEntity
 import org.springframework.web.client.postForEntity
 import java.util.*
@@ -63,6 +63,54 @@ class ExamControllerIntegrationTest {
 
         assertEquals(HttpStatus.CREATED, result.statusCode)
         assertEquals(Exam(name = "name-0", durationInMinutes = 10, startTime = Date(6000), course = "APP", version = 1, examType = ExamType.EXAM)
+                , result.body)
+    }
+
+    @Test
+    fun testAddQuestionToExam() {
+        val headers = HttpHeaders()
+        val requestJson = """{
+                                "examId":1,
+                                "name":"name-0",
+                                "durationInMinutes":10,
+                                "startTime":"1970-01-01T00:00:06.000+0000",
+                                "course":"APP",
+                                "examType":"EXAM",
+                                "instructions":null,
+                                "location":null,
+                                "questions":[
+                                {
+                                    "questionId": 1,
+                                    "questionText": "Openvraag text",
+                                    "questionType": "OPEN_QUESTION",
+                                    "course": "APP",
+                                    "subId": null,
+                                    "examType": "EXAM",
+                                    "subQuestions": null
+                                    }
+                                ]
+                            }"""
+
+        headers.contentType = MediaType.APPLICATION_JSON
+        val entity = HttpEntity(requestJson, headers)
+        val result = restTemplate.exchange<Exam>("http://localhost:$port/exam", HttpMethod.PUT, entity)
+
+        assertNotNull(result)
+        assertEquals(result.statusCode, HttpStatus.ACCEPTED)
+        assertEquals(Exam(
+                examId = 1,
+                name = "name-0",
+                durationInMinutes = 10,
+                startTime = Date(6000),
+                course = "APP",
+                examType = ExamType.EXAM,
+                questions = arrayOf(
+                        Question(1,
+                                "Openvraag text",
+                                QuestionType.OPEN_QUESTION,
+                                "APP",
+                                null, ExamType.EXAM,
+                                null)))
                 , result.body)
     }
 }
