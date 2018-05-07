742c6e25321d0f9a28d08e3df6179ee31eb1462a
YN.Martis@student.han.nl <YN.Martis@student.han.nl>
2018-05-07 12:03:12 +0200

    Added kDoc

--- src/main/kotlin/nl.han.ica/examplatform/controllers/responseexceptions/InvalidExamException.kt
+++ src/main/kotlin/nl.han.ica/examplatform/controllers/responseexceptions/InvalidExamException.kt
@@ -2,6 +2,13 @@ package nl.han.ica.examplatform.controllers.responseexceptions
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
+import nl.han.ica.examplatform.models.exam.Exam
 
+/**
+ * Exception thrown when there is an error in [Exam]
+ * It is bound to a HTTP status [HttpStatus.BAD_REQUEST]
+ *
+ * @property message [String] Message in exception
+ */
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 class InvalidExamException(message: String) : RuntimeException(message)
