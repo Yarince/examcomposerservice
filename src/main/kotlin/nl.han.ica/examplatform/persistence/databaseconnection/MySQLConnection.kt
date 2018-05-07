6f92abebd9cc45e5304daf5e392aef5e8500f642
Milo Tolboom <milotolboom@gmail.com>
2018-05-07 15:45:58 +0200

    kdoc mysqlconnection

---
+++ src/main/kotlin/nl.han.ica/examplatform/persistence/databaseconnection/MySQLConnection.kt
@@ -0,0 +1,116 @@
+package nl.han.ica.examplatform.persistence.databaseconnection
+
+import java.io.FileReader
+import java.sql.*
+import java.util.Properties
+import java.sql.DriverManager
+import java.sql.SQLException
+
+/**
+ * A singleton object that handles the connection with the MySQL database
+ **
+ * @property databaseProperties [Properties] holds the databaseproperties, such as the connection URL, username and password.
+ * @property establishedDatabaseConnection [Connection] Optional that can contain a database connection
+ * @constructor Initialises the database-properties.
+ */
+object MySQLConnection {
+
+    private val databaseProperties: Properties
+
+    init {
+        databaseProperties = initializeProperties()
+    }
+
+    /*
+    todo: In case that the database + the queries are ready to use this class has to be extended.
+    todo: It needs 2 more variables(preparedStatement+resultSet) and an extension of the getConnection method to retrieve data by prepared statements.
+    */
+    private var establishedDatabaseConnection: Connection? = null
+
+
+    /**
+     * Establishes a connection and fills the [establishedDatabaseConnection] property with that connection
+     */
+    private fun establishDatabaseConnection() {
+        try {
+            establishedDatabaseConnection = connectDatabase(getDatabaseConnectionUrl(databaseProperties), getDatabaseUsername(databaseProperties), getDatabasePassword(databaseProperties), getDrivers(databaseProperties))
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * Retrieves a connection and returns it
+     * @return a database [Connection] on which queries can be executed
+     */
+    fun getConnection(): Connection? {
+        establishDatabaseConnection()
+        return establishedDatabaseConnection
+    }
+
+    /**
+     * Reads the database properties, loads them into a [Properties] object and returns them
+     * @return loaded database properties
+     */
+    private fun initializeProperties(): Properties {
+        val databaseProperties = Properties()
+        val reader = FileReader(System.getProperty("user.dir") + "/src/main/kotlin/nl.han.ica/examplatform/config/databaseconfig/application.properties")
+        databaseProperties.load(reader)
+        return databaseProperties
+    }
+
+    /**
+     * Handles the database connection using the MySQL driver
+     * @param connectionURL the URL of the DB that should be connected with
+     * @param username the username that will be used to connect
+     * @param password the corresponding password of the user
+     * @param drivers the database drivers that are being used to connect
+     * @return a MySQL database [Connection]
+     */
+    private fun connectDatabase(connectionURL: String, username: String, password: String, drivers: String): Connection {
+        Class.forName(drivers)
+        return DriverManager.getConnection(connectionURL, username, password)
+    }
+
+    /**
+     * Closes the a database connection
+     * @param connectionToClose [Connection] that should be closed
+     */
+    fun closeConnection(connectionToClose: Connection?) {
+        try {
+            connectionToClose?.close()
+            establishedDatabaseConnection = null
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * Closes a prepared statement
+     * @param stmt [Statement] that should be closed
+     */
+    fun closeStatement(stmt: Statement) {
+        try {
+            stmt.close()
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun getDatabaseConnectionUrl(properties: Properties): String {
+        return properties.getProperty("jdbc.url")
+    }
+
+    private fun getDatabaseUsername(properties: Properties): String {
+        return properties.getProperty("jdbc.username")
+    }
+
+    private fun getDatabasePassword(properties: Properties): String {
+        return properties.getProperty("jdbc.password")
+    }
+
+    private fun getDrivers(properties: Properties): String {
+        return properties.getProperty("jdbc.driverClassName")
+    }
+
+}
