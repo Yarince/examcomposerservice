884bb9157f7a2c4d86920f5374584a65037dcca0
Arthur Baars <A.Baars@student.han.nl>
2018-05-07 13:37:27 +0200

    Added sql script to delete tables + insert data for tests.

--- src/main/kotlin/nl.han.ica/examplatform/Application.java
+++ src/main/kotlin/nl.han.ica/examplatform/Application.java
@@ -1,14 +1,19 @@
 package nl.han.ica.examplatform;
 
 
+import nl.han.ica.examplatform.persistence.databaseconnection.MySQLConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.sql.Connection;
 
 @SpringBootApplication
 @EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        Connection con = MySQLConnection.INSTANCE.getConnection();
     }
 }
