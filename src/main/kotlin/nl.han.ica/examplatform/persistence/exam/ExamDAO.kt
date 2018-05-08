37fa33d0f51084cdb8e5df5bdb09c006e6d08c68
Milo Tolboom <milotolboom@gmail.com>
2018-05-08 13:45:58 +0200

    fixed yarince's ugly todo

--- src/main/kotlin/nl.han.ica/examplatform/persistence/exam/ExamDAO.kt
+++ src/main/kotlin/nl.han.ica/examplatform/persistence/exam/ExamDAO.kt
@@ -110,7 +110,7 @@ class ExamDAO {
         preparedStatement = conn?.prepareStatement(insertExamQuery)
         preparedStatement?.setInt(1, exam.courseId.value)
         preparedStatement?.setInt(2, exam.examType.examId)
-        preparedStatement?.setString(3, exam.name)//TODO BRUH
+        preparedStatement?.setString(3, exam.name) // todo: discuss this with database team about examcode, change later
         preparedStatement?.setString(4, exam.name)
         preparedStatement?.setDate(5, java.sql.Date(exam.startTime.time))
         preparedStatement?.setDate(6, java.sql.Date(exam.endTime.time))
