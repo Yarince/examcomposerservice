4039177c9476691ba922bfd9041ebac9751f879a
Arthur Baars <A.Baars@student.han.nl>
2018-05-08 14:49:04 +0200

    Removed unused import.

--- unittest/nl/han/ica/examplatform/controllers/exam/ExamControllerTest.kt
+++ unittest/nl/han/ica/examplatform/controllers/exam/ExamControllerTest.kt
@@ -9,7 +9,6 @@ import nl.han.ica.examplatform.models.course.CourseType
 import nl.han.ica.examplatform.models.question.Question
 import nl.han.ica.examplatform.models.question.QuestionType
 import org.junit.Assert.*
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
