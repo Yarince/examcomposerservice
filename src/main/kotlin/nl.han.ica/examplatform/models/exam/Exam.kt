bfa90d7631413f76c781d480a01c3bf8f8b42907
Milo Tolboom <milotolboom@gmail.com>
2018-05-14 16:58:24 +0200

    removed exam interface because it wasnt useful

--- src/main/kotlin/nl.han.ica/examplatform/models/exam/Exam.kt
+++ src/main/kotlin/nl.han.ica/examplatform/models/exam/Exam.kt
@@ -8,7 +8,7 @@ data class Exam(
         @ApiModelProperty(notes = "The ID of the exam")
         val examId: Int? = null,
         @ApiModelProperty(notes = "The name of the exam", required = true, allowEmptyValue = false)
-        override val name: String,
+        val name: String,
         @ApiModelProperty(notes = "The duration in minutes of the exam", required = true)
         val durationInMinutes: Int,
         @ApiModelProperty(notes = "The start time of the exam", required = true)
@@ -16,15 +16,15 @@ data class Exam(
         @ApiModelProperty(notes = "The end time of the exam")
         val endTime: Date = Date(startTime.time + durationInMinutes),
         @ApiModelProperty(notes = "The ID of the course that the exam is for", required = true)
-        override val courseId: Int,
+        val courseId: Int,
         @ApiModelProperty(notes = "The version of the exam")
-        override val version: Int = 1,
+        val version: Int = 1,
         @ApiModelProperty(notes = "The type of the exam can be practice test or exam")
-        override val examType: ExamType,
+        val examType: ExamType,
         @ApiModelProperty(notes = "The instructions for the exam")
         val instructions: String? = null,
         @ApiModelProperty(notes = "The location of the exam")
         val location: String? = null,
         @ApiModelProperty(notes = "The questions in the exam")
-        override val questions: ArrayList<Question>? = null
-): IExam
+        val questions: ArrayList<Question>? = null
+)
--- src/main/kotlin/nl.han.ica/examplatform/models/exam/PracticeExam.kt
+++ src/main/kotlin/nl.han.ica/examplatform/models/exam/PracticeExam.kt
@@ -5,13 +5,13 @@ import nl.han.ica.examplatform.models.question.Question
 
 data class PracticeExam(
         @ApiModelProperty(notes = "The name of the exam", required = true, allowEmptyValue = false)
-        override val name: String,
+        val name: String,
         @ApiModelProperty(notes = "The ID of the course that the exam is for", required = true)
-        override val courseId: Int,
+        val courseId: Int,
         @ApiModelProperty(notes = "The version of the practice exam")
-        override val version: Int? = 1,
+        val version: Int? = 1,
         @ApiModelProperty(notes = "The type of the exam can be practice test or exam")
-        override val examType: ExamType = ExamType.PRACTICE_EXAM,
+        val examType: ExamType = ExamType.PRACTICE_EXAM,
         @ApiModelProperty(notes = "The questions in the exam")
-        override val questions: ArrayList<Question>
-): IExam
+        val questions: ArrayList<Question>
+)
