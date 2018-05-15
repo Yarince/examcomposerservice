69970042fd9b3fd00c011ad3c6c7e864014115fb
Milo Tolboom <milotolboom@gmail.com>
2018-05-15 13:39:35 +0200

    more expansive unittest for algoritm

--- unittest/nl/han/ica/examplatform/business/exam/PracticeExamGeneratorTest.kt
+++ unittest/nl/han/ica/examplatform/business/exam/PracticeExamGeneratorTest.kt
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.doReturn
 import org.mockito.junit.MockitoJUnitRunner
+import kotlin.test.assertTrue
 
 
 @RunWith(MockitoJUnitRunner::class)
@@ -20,17 +21,17 @@ internal class PracticeExamGeneratorTest {
     @Test
     fun testGenerateExamSuccess() {
         val courseId = 1
-        val categories = arrayOf("QA", "DCAR", "ATAM")
+        val categories = arrayOf("ATAM", "DCAR", "SRS")
         val expectedQuestions = arrayOf(
-                Question(courseId = courseId, categories = arrayOf("QA", "ATAM")),
-                Question(courseId = courseId, categories = arrayOf("QA")),
-                Question(courseId = courseId, categories = arrayOf("QA", "ASR")),
-                Question(courseId = courseId, categories = arrayOf("ASR")),
-                Question(courseId = courseId, categories = arrayOf("ASR", "DCAR", "QA")),
-                Question(courseId = courseId, categories = arrayOf("DCAR")),
-                Question(courseId = courseId, categories = arrayOf("DCAR")),
-                Question(courseId = courseId, categories = arrayOf("DCAR")),
-                Question(courseId = courseId, categories = arrayOf("DCAR"))
+                Question(courseId = courseId, questionText = "Question 1", categories = arrayOf("QA", "ATAM")),
+                Question(courseId = courseId, questionText = "Question 2", categories = arrayOf("QA")),
+                Question(courseId = courseId, questionText = "Question 3", categories = arrayOf("QA", "ASR")),
+                Question(courseId = courseId, questionText = "Question 4", categories = arrayOf("ASR")),
+                Question(courseId = courseId, questionText = "Question 5", categories = arrayOf("ASR", "DCAR", "QA")),
+                Question(courseId = courseId, questionText = "Question 6", categories = arrayOf("DCAR")),
+                Question(courseId = courseId, questionText = "Question 7", categories = arrayOf("DCAR")),
+                Question(courseId = courseId, questionText = "Question 8", categories = arrayOf("DCAR")),
+                Question(courseId = courseId, questionText = "Question 9", categories = arrayOf("DCAR"))
         )
 
         doReturn(expectedQuestions).`when`(questionDAO).getQuestions(courseId, categories)
@@ -40,5 +41,16 @@ internal class PracticeExamGeneratorTest {
         assertNotNull(result)
         assertEquals(courseId, result.courseId)
         assertEquals(expectedQuestions.size / 2, result.questions.size)
+
+        // Check if all categories are in the practice exam
+        val filteredQuestions: MutableMap<String, List<Question>> = mutableMapOf()
+        for (category in categories) {
+            filteredQuestions[category] = result.questions.filter { it.categories.contains(category) }
+        }
+        val allCategoriesInResult = filteredQuestions.keys.toTypedArray()
+        assertTrue(categories.contentEquals(allCategoriesInResult))
+
+        // Check if there are duplicate questions
+        assertEquals(result.questions.size, result.questions.distinctBy { Pair(it.questionText, it.categories) }.size)
     }
 }
