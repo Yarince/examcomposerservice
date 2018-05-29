66cb0fa4bdb9c73ea7151edf62e05ad3c64ce69e
Milo Tolboom <milotolboom@gmail.com>
2018-05-29 11:42:30 +0200

    added controller function for questions per course

--- src/main/kotlin/nl.han.ica/examplatform/controllers/question/QuestionController.kt
+++ src/main/kotlin/nl.han.ica/examplatform/controllers/question/QuestionController.kt
@@ -2,12 +2,11 @@ package nl.han.ica.examplatform.controllers.question
 
 import io.swagger.annotations.*
 import nl.han.ica.examplatform.business.question.QuestionService
+import nl.han.ica.examplatform.models.exam.Exam
 import nl.han.ica.examplatform.models.question.Question
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @Api("question", description = "Creating, updating and deleting questions")
@@ -23,4 +22,13 @@ class QuestionController {
             ApiResponse(code = 500, message = "Internal server error")
     )
     fun createQuestion(@ApiParam(value = "Question object", required = true) @RequestBody question: Question): ResponseEntity<Question> = questionService.addQuestion(question)
+
+    @GetMapping("/{courseName}")
+    @ApiOperation(value = "Retrieve questions of a course", notes = "Retrieve questions that are within a specific course", response = Exam::class)
+    @ApiResponses(
+            ApiResponse(code = 201, message = "Create"),
+            ApiResponse(code = 403, message = "Bad request"),
+            ApiResponse(code = 404, message = "Not found"))
+    fun getExam(@ApiParam(value = "The name of the course you want to retrieve the questions of", required = true) @PathVariable("courseName") courseName: Int): ResponseEntity<Exam> =
+            questionService.getQuestionsForCourse(courseName)
 }
