908386d9b66337435c6b05c71a4c8a92748230ba
Milo Tolboom <milotolboom@gmail.com>
2018-05-30 12:23:46 +0200

    removed whitespace

--- src/main/kotlin/nl.han.ica/examplatform/business/exam/ExamService.kt
+++ src/main/kotlin/nl.han.ica/examplatform/business/exam/ExamService.kt
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
-
 /**
  * Exam service for handling requests related to the Exam model.
  */
