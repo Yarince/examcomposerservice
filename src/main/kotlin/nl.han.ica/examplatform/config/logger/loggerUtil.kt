dee4cc3f81471b4295444d50c7a2e5b359ef5f44
Milo Tolboom <milotolboom@gmail.com>
2018-05-30 11:17:05 +0200

    added enter specially for yarince

--- src/main/kotlin/nl.han.ica/examplatform/config/logger/loggerUtil.kt
+++ src/main/kotlin/nl.han.ica/examplatform/config/logger/loggerUtil.kt
@@ -3,5 +3,4 @@ package nl.han.ica.examplatform.config.logger
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-//inline fun <reified T:Any> loggerFor(): Logger = LoggerFactory.getLogger(T::class.java.name)
 fun <T> loggerFor(clazz: Class<T>): Logger = LoggerFactory.getLogger(clazz)
--- src/main/kotlin/nl.han.ica/examplatform/persistence/answer/AnswerDAO.kt
+++ src/main/kotlin/nl.han.ica/examplatform/persistence/answer/AnswerDAO.kt
@@ -14,6 +14,7 @@ import java.sql.SQLException
  */
 @Repository
 class AnswerDAO : IAnswerDAO {
+
     private val logger = loggerFor(javaClass)
 
     /**
