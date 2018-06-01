61ded547f1a0be07916333c9a5bb483009f0632f
Arthur Baars <A.Baars@student.han.nl>
2018-06-01 11:42:36 +0200

    Fixed removed kDoc

--- src/main/kotlin/nl/han/ica/examplatform/persistence/databaseconnection/MySQLConnection.kt
+++ src/main/kotlin/nl/han/ica/examplatform/persistence/databaseconnection/MySQLConnection.kt
@@ -12,6 +12,9 @@ import java.sql.SQLException
 import java.sql.Statement
 import java.util.*
 
+/**
+ * A singleton object that handles the connection with the MySQL database.
+ */
 object MySQLConnection {
 
     /**
