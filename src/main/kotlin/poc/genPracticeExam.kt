310980902ece3a84369ebb236dd678d68723e665
Milo Tolboom <milotolboom@gmail.com>
2018-06-03 14:21:46 +0200

    rating of categories stubbed

--- src/main/kotlin/poc/genPracticeExam.kt
+++ src/main/kotlin/poc/genPracticeExam.kt
@@ -12,10 +12,29 @@ data class Question(val questionId: Int, val questionText: String, val categorie
 
 fun generateExam() {
     val questions = loadQuestions()
-    questions.forEach { println(it) }
+    val ratedCategories = questionsToCategoryRating(questions)
+    ratedCategories.forEach { println(it) }
 }
 
 private fun loadQuestions(): Array<Question> {
     val reader = JsonReader(FileReader("src/main/kotlin/poc/resources/questionBankNotAnswered.json"))
     return Gson().fromJson(reader, Array<Question>::class.java)
+}
+
+private fun questionsToCategoryRating(questions: Array<Question>): HashMap<String, Double> {
+    // This will be implemented by another team member, so this is a stub that returns the categories and ratings
+    val categories = ArrayList<String>()
+    for (question in questions) {
+        for (category in question.categories) {
+            if (!categories.contains(category))
+                categories.add(category)
+        }
+    }
+
+    val ratedCategories = HashMap<String, Double>()
+    categories.forEachIndexed { index, category ->
+        ratedCategories[category] = (index + 1) * 25.0
+    }
+
+    return ratedCategories
 }
