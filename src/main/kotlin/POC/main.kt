ae8671439f855b9414ef48fdfe01fb407f58ce7c
Milo Tolboom <milotolboom@gmail.com>
2018-06-11 14:04:55 +0200

    added arthurs code

--- src/main/kotlin/POC/main.kt
+++ src/main/kotlin/POC/main.kt
@@ -1,4 +1,4 @@
-package POC
+package poc
 
 import com.google.gson.Gson
 import com.google.gson.stream.JsonReader
--- src/main/kotlin/poc/questionLoader.kt
+++ src/main/kotlin/poc/questionLoader.kt
@@ -8,11 +8,11 @@ import java.io.FileReader
 
 internal fun loadQuestions(courseId: Int, studentNr: Int, type: String): Array<Question> {
     // Here the DB should get questions for courseId and studentNr
-    val reader = JsonReader(FileReader("src/main/kotlin/poc/resources/$type.json"))
+    val reader = JsonReader(FileReader("src/main/kotlin/pocmilo/resources/$type.json"))
     return Gson().fromJson(reader, Array<Question>::class.java)
 }
 
 internal fun loadResultsOfOthers(category: String): Array<QuestionResultStats> {
-    val reader = JsonReader(FileReader("src/main/kotlin/poc/resources/questionsMadeByOtherStudents.json"))
+    val reader = JsonReader(FileReader("src/main/kotlin/pocmilo/resources/questionsMadeByOtherStudents.json"))
     return Gson().fromJson(reader, Array<QuestionResultStats>::class.java)
 }
