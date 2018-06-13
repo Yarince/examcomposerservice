91b38ae7e4746a5418cf0e13bc99aad87c27db66
Arthur Baars <A.Baars@student.han.nl>
2018-06-13 16:20:16 +0200

    Removed unnecessary code

--- src/main/java/nl/han/ica/examplatform/Application.java
+++ src/main/java/nl/han/ica/examplatform/Application.java
@@ -1,8 +1,6 @@
 package nl.han.ica.examplatform;
 
 
-import nl.han.ica.examplatform.models.question.Question;
-import nl.han.ica.examplatform.persistence.question.QuestionDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,19 +8,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-
 @SpringBootApplication
 @EnableSwagger2
 public class Application implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        QuestionDAO q = new QuestionDAO();
-        ArrayList<String> a = new ArrayList<>();
-        a.add("nee");
-        a.add("ja");
-        q.insertQuestion(new Question(18, null, "OpenQuestion", "MultipleChoice", "toetsie", 5 , 2, "Tentamen", "MultipleChoice", "1.0", "1.0", a, null), null);
     }
 
     @Bean
