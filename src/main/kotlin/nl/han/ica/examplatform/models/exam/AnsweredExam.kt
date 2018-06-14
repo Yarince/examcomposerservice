b71d1326e4af10753bfb076bc2cd1e9364c02110
Robin Schuiling <JR.Schuiling@student.han.nl>
2018-06-14 13:07:08 +0200

    BTGGOM-548:  - Formatted code

--- src/main/kotlin/nl/han/ica/examplatform/models/exam/AnsweredExam.kt
+++ src/main/kotlin/nl/han/ica/examplatform/models/exam/AnsweredExam.kt
@@ -3,7 +3,7 @@ package nl.han.ica.examplatform.models.exam
 import nl.han.ica.examplatform.models.question.AnsweredQuestion
 import java.util.*
 
-class AnsweredExam (
+class AnsweredExam(
         val examId: Int,
         val examDate: Date,
         val answeredQuestions: Array<AnsweredQuestion>
