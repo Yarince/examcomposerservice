083f43f5b0312980eb15fd3735baf57d254c67fa
YN.Martis@student.han.nl <YN.Martis@student.han.nl>
2018-06-18 12:11:59 +0200

    Added exception for if code is null

--- src/main/kotlin/nl/han/ica/examplatform/persistence/decryptioncode/DecryptionCodeDAO.kt
+++ src/main/kotlin/nl/han/ica/examplatform/persistence/decryptioncode/DecryptionCodeDAO.kt
@@ -2,6 +2,7 @@ package nl.han.ica.examplatform.persistence.decryptioncode
 
 import nl.han.ica.examplatform.config.logger.loggerFor
 import nl.han.ica.examplatform.controllers.DatabaseException
+import nl.han.ica.examplatform.controllers.decryptioncode.DecryptionCodeNotFoundException
 import nl.han.ica.examplatform.persistence.databaseconnection.MySQLConnection
 import org.springframework.stereotype.Repository
 import java.sql.Connection
@@ -29,10 +30,11 @@ class DecryptionCodeDAO : IDecryptionCodeDAO {
         val preparedStatement: PreparedStatement? = conn?.prepareStatement(sqlRetrieveExamDecryptKeyQuery)
         preparedStatement?.setInt(1, examId)
 
-        return try{
-            val resultSet = preparedStatement?.executeQuery()?: throw DatabaseException("Error while fetching decryption codes from the database.")
+        return try {
+            val resultSet = preparedStatement?.executeQuery()
+                    ?: throw DatabaseException("Error while fetching decryption codes from the database.")
             resultSet.next()
-            resultSet.getString("DECRYPTKEY")
+            resultSet.getString("DECRYPTKEY") ?: throw DecryptionCodeNotFoundException()
         } catch (e: SQLException) {
             logger.error("Error retrieving exam decryption key $examId", e)
             throw DatabaseException("Error while retrieving exam decryption key $examId")
