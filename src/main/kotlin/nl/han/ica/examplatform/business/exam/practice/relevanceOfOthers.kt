6069abc425fc084e5799069599fb4cdb84a9a15d
Milo Tolboom <milotolboom@gmail.com>
2018-06-19 12:48:00 +0200

    fixed relevance of others

--- src/main/kotlin/nl/han/ica/examplatform/business/exam/practice/relevanceOfOthers.kt
+++ src/main/kotlin/nl/han/ica/examplatform/business/exam/practice/relevanceOfOthers.kt
@@ -17,20 +17,26 @@ internal fun getMostRelevantNotAssessedQuestionOfCategory(category: String, ques
     val assessedQuestionsOfOthers: ArrayList<QuestionResultStats> = examResultsDAO.getResultsOfOthersInCategory(studentNr, category)
 
     // Filter questions to only contain current category
-    val questionIdsOfCategory = questions.filter { it.categories.contains(category) }.map { it.questionId }
+    val questionsOfCategory = questions.filter { it.categories.contains(category) }
+    val questionIdsOfCategory = questionsOfCategory.map { it.questionId }
     val filteredQuestions = assessedQuestionsOfOthers.filter { questionIdsOfCategory.contains(it.questionId) }
 
     var mostRelevantQuestion: Pair<Int, Double>? = null
 
-    filteredQuestions.forEach {
-        // The lower the rating, the more relevant the question is because of the low score on the question
-        val rating = it.nGood.toDouble() / it.nResults
+    if (filteredQuestions.isEmpty() && questionsOfCategory.isNotEmpty()) {
+        // add question that hasn't been answered by anyone
+        mostRelevantQuestion = Pair(questionsOfCategory[0].questionId!!, 100.0)
+    } else {
+        filteredQuestions.forEach {
+            // The lower the rating, the more relevant the question is because of the low score on the question
+            val rating = it.nGood.toDouble() / it.nResults
 
-        if (mostRelevantQuestion == null)
-            mostRelevantQuestion = Pair(it.questionId, rating)
-        else
-            if (rating < mostRelevantQuestion!!.second)
+            if (mostRelevantQuestion == null)
                 mostRelevantQuestion = Pair(it.questionId, rating)
+            else
+                if (rating < mostRelevantQuestion!!.second)
+                    mostRelevantQuestion = Pair(it.questionId, rating)
+        }
     }
 
     return questions.find { it.questionId == mostRelevantQuestion?.first }
